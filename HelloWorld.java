f389920c2e792c51b85cc8ea41ff72ba90a5d46c
abhishek manocha <abhishek@abhishek-3000-N100.(none)>
2014-04-20 09:08:52 +0530

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Committer: abhishek manocha <abhishek@abhishek-3000-N100.(none)>
    
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	new file:   HelloWorld.java
    	new file:   ctest.c

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+public class HelloWorld {
+
+public interface CTest extends Library{
+	public void helloFromC();
+}
+	public static void main(String[] args) {
+		CTest ctest = (CTest) Native.loadLibrary("ctest", CTest.class);
+		ctest.helloFromC();
+	}
+}
